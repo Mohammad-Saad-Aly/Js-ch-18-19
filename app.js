417b0f4400a185e3ec5b4fb9cf00c9e4ae3861a1
Mohammad Saad Ali <52074642+Mohammad-Saad-Aly@users.noreply.github.com>
2021-04-02 21:03:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,57 @@
+// var name1=["atif","yasir","safwan","abdullah","saad"]
+// var x =name1.slice(0,2)
+// console.log (x)
+
+
+// for
+// while
+// do while
+
+// var num1 = 1
+// var num1 = 2
+// var num1 = 3
+// var num1 = 4     bad practice
+// var num1 = 5
+// var num1 = 6
+// var num1 = 7
+// var num1 = 8
+
+// for(var i=0; i>0; i++){
+// console.log(i)
+//     document.write(i)
+// }
+
+// var i=999;
+// for (var j = 1; j <= 10; j++){
+//     document.write(i + " x " + j + " = " + i*j + "<br>")
+// }
+
+// var array = ["Saad","Ali","Karachi","Islamabad"];
+// for(var i=0; i<array.length; i++){
+//     if(array[i] === "Karachi"){
+//         alert("Karachi is Love")
+//     }
+// }
+// for(var i = 0; i<array.length; i++){
+//     document.write(array[i]+"<br>")
+// }
+// for(var i=0; i<=10; i++){
+//     if(i === 5){
+//         document.write(i)
+//         break
+//     }
+//     document.write(i)
+
+// }
+
+// var a = ["Karachi","Islamabad","lahore","multan"]
+// for(var i=0; i<a.length; i++){
+//     document.write(a[i]+"<br>")
+// }
+
+// var a = ["Karachi","Islamabad","lahore","multan"]
+// for(var i =0; ith<a.leng; i++){
+//     if(a[i] === "Saad"){
+//         document.write("Karachi is Love")
+//     }
+// }
